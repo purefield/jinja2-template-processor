61f951f478f2fafc3a33679394763c747da77bbe
Daniel Schimpfoessl <dds@redhat.com>
2025-12-26 15:37:28 -0600

    Adding steps to update clusterfile-editor on quay.io

--- clusterfile-editor.sh
+++ clusterfile-editor.sh
@@ -1,6 +1,7 @@
 cd apps/editor
 podman build -t clusterfile-editor .
+podman tag localhost/clusterfile-editor  quay.io/dds/clusterfile-editor
+podman push quay.io/dds/clusterfile-editor
 podman run -p 8000:8000 -v $(pwd)/../../templates:/app/templates \
                         -v $(pwd)/../../data:/app/samples \
-                        -v $(pwd)/../../schema:/app/schema clusterfile-editor
-
+                        -v $(pwd)/../../schema:/app/schema quay.io/dds/clusterfile-editor
