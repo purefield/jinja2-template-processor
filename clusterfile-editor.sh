cdd5973f37e0ea4e0c9ea71bbbd952e8c4faeb60
Daniel Schimpfoessl <dds@redhat.com>
2025-12-26 20:49:59 -0600

    Build clusterfile-editor with samples,templates and schema in the image

--- clusterfile-editor.sh
+++ clusterfile-editor.sh
@@ -1,7 +1,6 @@
-cd apps/editor
-podman build -t clusterfile-editor .
-podman tag localhost/clusterfile-editor  quay.io/dds/clusterfile-editor
-podman push quay.io/dds/clusterfile-editor
+# podman build -t clusterfile-editor -f apps/editor/Containerfile .
+# podman tag localhost/clusterfile-editor  quay.io/dds/clusterfile-editor
+# podman push quay.io/dds/clusterfile-editor
 podman run -p 8000:8000 -v $(pwd)/../../templates:/app/templates \
                         -v $(pwd)/../../data:/app/samples \
                         -v $(pwd)/../../schema:/app/schema quay.io/dds/clusterfile-editor
