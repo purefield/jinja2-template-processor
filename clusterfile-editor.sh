0b6c9b24f6415fa7755dcb7795dc9ad978d7f125
Daniel Schimpfoessl <dds@redhat.com>
2026-01-05 14:42:31 -0600

    Change into apps/editor before creating the containerized editor

--- clusterfile-editor.sh
+++ clusterfile-editor.sh
@@ -1,6 +1,7 @@
 # podman build -t clusterfile-editor -f apps/editor/Containerfile .
 # podman tag localhost/clusterfile-editor  quay.io/dds/clusterfile-editor
 # podman push quay.io/dds/clusterfile-editor
+cd apps/editor/
 podman run -p 8000:8000 -v $(pwd)/../../templates:/app/templates \
                         -v $(pwd)/../../data:/app/samples \
                         -v $(pwd)/../../schema:/app/schema quay.io/dds/clusterfile-editor
